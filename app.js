ff3c7b2ff30e3f53e9b4d67ba20c14b12893b709
Your Name <m.mahdi.nayyeri@gmail.com>
2022-09-14 14:01:53 +0430

    first project

---
+++ app.js
@@ -0,0 +1,13 @@
+function shomare(adad) {
+  if (adad == 1) console.log("adad morakab");
+  if (adad == 2) console.log("adad aval");
+  if (adad == 3) console.log("adad aval");
+  if (adad == 4) console.log("adad morakab");
+  if (adad == 5) console.log("adad aval");
+  if (adad == 6) console.log("adad morakab");
+  if (adad == 7) console.log("adad aval");
+  if (adad == 8) console.log("adad morakab");
+  if (adad == 9) console.log("adad morakab");
+  if (adad == 10) console.log("adad morakab");
+}
+shomare(3);
